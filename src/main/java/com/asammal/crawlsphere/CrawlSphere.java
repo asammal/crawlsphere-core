598b14f09534d5573278a5775f5a3eef284fddbf
asammal <alka.sammal@gmail.com>
2025-10-01 19:32:25 +0530

    pick seed from text file

--- src/main/java/com/asammal/crawlsphere/CrawlSphere.java
+++ src/main/java/com/asammal/crawlsphere/CrawlSphere.java
@@ -2,6 +2,7 @@ package com.asammal.crawlsphere;
 
 import com.asammal.crawlsphere.model.PageData;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,29 +16,33 @@ public class CrawlSphere {
         System.out.println("Enter topic to crawl:");
         String topic = scanner.nextLine().trim().toLowerCase();
 
-        /*System.out.println("Enter starting URLs (comma-separated):");
-        String urlInput = scanner.nextLine().trim();
-        scanner.close();
+        System.out.println("Let's look for " + topic);
 
-        String[] rawUrls = urlInput.split(",");
         List<String> startUrls = new ArrayList<>();
-        for (String url : rawUrls) {
-            url = url.trim();
-            if (!url.isEmpty()) {
-                startUrls.add(url);
+        InputStream inputStream = CrawlSphere.class.getClassLoader()
+                .getResourceAsStream("input/seed.txt");
+
+        if (inputStream == null) {
+            System.out.println("seed.txt not found in src/main/resources/input/");
+            return;
+        }
+
+        // Read seed URLs from seed.txt
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (!line.isEmpty()) startUrls.add(line);
             }
+        } catch (IOException e) {
+            System.out.println("Failed to read seed.txt: " + e.getMessage());
+            return;
         }
 
         if (startUrls.isEmpty()) {
             System.out.println("No valid starting URLs provided. Exiting.");
             return;
-        }*/
-
-        List<String> startUrls = Arrays.asList(
-                "https://en.wikipedia.org/wiki",
-                "https://www.ibm.com/topics/",
-                "https://www.quantamagazine.org/tag/"
-        );
+        }
 
         Crawler crawler = new Crawler(topic);
         for (String url : startUrls) {
